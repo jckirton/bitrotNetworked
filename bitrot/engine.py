e56b007e69b05983ec205b8f60628b66f6de269c
jckirton <66995801+jckirton@users.noreply.github.com>
2026-07-30 12:38:00 +0930

    bitrot engine documentation

--- bitrot/engine.py
+++ bitrot/engine.py
@@ -5,7 +5,13 @@ __all__ = ["Match", "Game"]
 
 
 class Game:
+    """A BITROT game.
+
+    Contains the board, game state, and methods for turns and state strings.
+    """
+
     def __init__(self):
+        """Create a BITROT game."""
         self.board = self.Board()
         self.playing = False
         self.recent_player = ""
@@ -13,7 +19,14 @@ class Game:
         self.winner: int | None = None
 
     class Board:
+        """A BITROT board.
+
+        Contains the current board state,
+        and methods for updating the board state.
+        """
+
         def __init__(self):
+            """Create a BITROT board."""
             self.state: dict[int, None | Game.Board.Piece] = {
                 1: None,
                 2: None,
@@ -27,7 +40,18 @@ class Game:
             }
 
         class Piece:
+            """A BITROT piece.
+
+            Contains an owner and age.
+            """
+
             def __init__(self, player: int, age: int = 0):
+                """Create a BITROT piece.
+
+                Args:
+                    player (int): The piece's owner. `0` is attacker, `1` is defender.
+                    age (int, optional): The age of the piece from 0-2. Defaults to 0.
+                """
                 self.p = player
                 self.a = age
                 self.AGE_COLORS = {
@@ -53,6 +77,11 @@ class Game:
                     return hex(15 - self.a)[2:]
 
         def age_pieces(self, player: int):
+            """Increment the age of a player's pieces on the board.
+
+            Args:
+                player (int): Which player's pieces to age.
+            """
             for pos in self.state:
                 piece = self.state[pos]
                 if isinstance(piece, self.Piece):
@@ -62,9 +91,27 @@ class Game:
                         self.state[pos] = None
 
         def add_piece(self, player: int, position: int):
+            """Add a new piece to the board at position.
+
+            Args:
+                player (int): The piece's player.
+                position (int): The position on the board to put the piece, from 1-9.
+            """
             self.state[position] = self.Piece(player)
 
-        def check_move(self, op: str):
+        def check_move(self, op: str) -> bool:
+            """Return if a given operation is valid.
+
+            Args:
+                op (str): The operation to check.
+
+            Raises:
+                Forfeit: The given operation is a forfeit (`"f"`).
+                NullOp: The given operation is null (`""` or `"0"`).
+                OutOfBounds: The given operation is beyond the valid board range (`op<1`, `op>9`).
+                OccupiedSpace: The given operation is a space that is already taken.
+                InvalidMove: The given operation is unknown or miscellaneously invalid.
+            """
             if op == "f":
                 raise Forfeit("a forfeit was declared.")
             if op in "0":
@@ -85,7 +132,24 @@ class Game:
             else:
                 return True
 
-        def parse(self, state: str, set: bool = False):
+        def parse(
+            self, state: str, set: bool = False
+        ) -> dict[int, None | Game.Board.Piece]:
+            """Parse a BITROT board state string into an internal board state.
+
+            If `set` is `True`, also sets `self.state` to the output.
+
+            Args:
+                state (str): The board state string to parse.
+                set (bool, optional): If `self.state` should be set to the parsed state. Defaults to False.
+
+            Raises:
+                UnknownState: The given state could not be parsed.
+
+            Returns:
+                dict[int, None | Game.Board.Piece]: The parsed state.
+            """
+
             temp_state: dict[int, None | Game.Board.Piece] = {
                 1: None,
                 2: None,
@@ -98,16 +162,23 @@ class Game:
                 9: None,
             }
             i = 1
-            for pos in state:
-                piece = int(pos, base=16)
-                if 1 <= piece <= 3:
-                    temp_state[i] = self.Piece(0, piece - 1)
-                elif 13 <= piece <= 15:
-                    temp_state[i] = self.Piece(1, abs(piece - 15))
-                i += 1
-            if set:
-                self.state = temp_state
-            return temp_state
+            try:
+                for pos in state:
+                    piece = int(pos, base=16)
+                    if 1 <= piece <= 3:
+                        temp_state[i] = self.Piece(0, piece - 1)
+                    elif 13 <= piece <= 15:
+                        temp_state[i] = self.Piece(1, abs(piece - 15))
+                    elif piece == 0:
+                        pass
+                    else:
+                        raise ValueError(f"invalid piece identifier '{pos}'")
+                    i += 1
+                if set:
+                    self.state = temp_state
+                return temp_state
+            except Exception:
+                raise UnknownState(f"erroneous/unknown board state '{state}'")
 
         def __str__(self):
             from copy import copy
@@ -137,6 +208,12 @@ class Game:
             return format(self) == format(value)
 
     def check_win(self) -> int | None:
+        """Return if a player has won based on the current game state.
+
+        Returns:
+            int: The winning player. `0` for attacker, `1` for defender.
+            None: No player has met a win condition.
+        """
         if self.recent_op == "f":
             return self.winner
 
@@ -168,6 +245,15 @@ class Game:
         return None
 
     def do_turn(self, player: int, op: str):
+        """Do a BITROT turn.
+
+        Does all the components of checking validity and updating game state,
+        including checking for a winning player.
+
+        Args:
+            player (int): The acting player.
+            op (str): The player's operation.
+        """
         self.recent_player: str = str(player)
         self.recent_op: str = op
         try:
@@ -209,6 +295,19 @@ class Game:
             # print(f"{self.winner} has won.")
 
     def parse_net_state(self, net_state: str):
+        """Parse a given BITROT game state string.
+
+        Generally used for doing the networked opponents turn,
+        or recieving initial state.
+
+        Args:
+            net_state (str): The BITROT state string to parse.
+
+        Raises:
+            Forfeit: The operation was a forfeit.
+            DesyncError: The two clients' states have become desynced.
+            GameAbort: The opposing client crashed and the active game is dead.
+        """
         net_playing = net_state[0]
         net_board = net_state[1:10]
         net_p = net_state[10]
@@ -243,29 +342,41 @@ class Game:
 
     @staticmethod
     def instance_from_state(state: str) -> Game:
-        playing = state[0]
-        board = state[1:10]
-        p = state[10]
-        op = state[11]
+        """Return a new Game instance from a given BITROT state string.
 
-        new_instance = Game()
-        new_instance.board.parse(board, True)
-        new_instance.recent_player = p
-        new_instance.recent_op = op
+        Args:
+            state (str): The BITROT state string.
 
-        if playing in "01":
-            new_instance.playing = bool(int(playing))
-            new_instance.winner = None
-        elif playing in "23":
-            new_instance.playing = False
-            new_instance.winner = int(playing) - 2
-        else:
+        Raises:
+            UnknownState: The given state is unknown or not a valid game state.
+        """
+        try:
+            playing = state[0]
+            board = state[1:10]
+            p = state[10]
+            op = state[11]
+
+            new_instance = Game()
+            new_instance.board.parse(board, True)
+            new_instance.recent_player = p
+            new_instance.recent_op = op
+
+            if playing in "01":
+                new_instance.playing = bool(int(playing))
+                new_instance.winner = None
+            elif playing in "23":
+                new_instance.playing = False
+                new_instance.winner = int(playing) - 2
+            else:
+                raise UnknownState(f"unknown/invalid playing state '{playing}'")
+
+            return new_instance
+        except Exception:
             raise UnknownState(f"erroneous/unknown state '{state}'")
-
-        return new_instance
 
     @staticmethod
     def generate_initial_state() -> str:
+        """Return a randomised valid initial BITROT state string."""
         from random import sample
 
         moves = sample(range(1, 10), k=2)
@@ -288,6 +399,11 @@ class Game:
 
 
 class Match:
+    """A BITROT match.
+
+    Contains a Game instance, a given Networking instance, player information,
+    methods for doing local/networked turns, and communicating state."""
+
     from .networking import Networking
 
     def __init__(
@@ -298,6 +414,17 @@ class Match:
         team: int,
         state: str | None = None,
     ):
+        """Create a new BITROT match with the given information.
+
+        Args:
+            network (Networking): The current `bitrot.Networking` instance.
+            user (str): The name of the user the local user is playing as.
+            opponent (str): The name of the opponent's user,
+            team (int): The team of the local user. `0` is attacker, `1` is defender.
+            state (str | None, optional): The initial game state.
+              Needed when the initial state is given by the opponent.
+              Defaults to None.
+        """
         self.game: Game
         self.network = network
         self.user = user
@@ -315,15 +442,26 @@ class Match:
             self.network.in_game = True
 
     def send_state(self) -> Networking.Response:
+        """Send the current game state to the opponent.
+
+        Returns:
+            Networking.Response: The response of the `Network.tell` call.
+        """
         # return self.network.tell(self.user, self.opponent, format(self.game))
         return self.network.tell(
             self.network.curr_user, self.opponent, format(self.game)
         )
 
     def do_self_turn(self, op: str):
+        """Do local turn.
+
+        Args:
+            op (str): The submitted operation.
+        """
         self.game.do_turn(self.team, op)
 
     def do_opponent_turn(self):
+        """Listen and perform networked opponent turn."""
         done = False
 
         while not done:
